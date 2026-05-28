8980cbfd97dd5592e636fb5d22f8cddaccac908e
Harendrasinh <haren111990@gmail.com>
2026-05-28 19:39:09 +0530

    update award title for page

--- src/app/awards/page.js
+++ src/app/awards/page.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const AwardsPage = () => {
   return (
-    <div>AwardsPage</div>
+    <div>
+        <div className="text-2xl font-bold bg-sky-800 text-white p-4 text-center">Our Awards Area</div>
+    </div>
   )
 }
 
