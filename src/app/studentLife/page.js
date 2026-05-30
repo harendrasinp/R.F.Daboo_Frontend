fcf0fe81f6f9f205aa9c7d8a84580d050775dcab
harendrsainh <haren111990@gmail.com>
2026-05-30 22:19:24 +0530

    update studenst life page

--- src/app/studentLife/page.js
+++ src/app/studentLife/page.js
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import DiamondImage from '../../utils/diamond';
 const StudentsLife = () => {
-  return (
-    <div>StudentsLife</div>
-  )
+    return (
+        <div className="flex flex-col items-center mb-3">
+            <div className="relative bg-linear-to-bl from-pink-700 to-sky-300 w-200 p-2 h-125 ">
+            
+                    <div className="absolute top-10 left-10">
+                        <DiamondImage image="/hero/img1.jpeg" />
+                    </div>
+
+                    <div className="absolute top-39 left-39">
+                        <DiamondImage image="/hero/img2.jpeg" />
+                    </div>
+                    <div className="absolute top-68 left-10">
+                        <DiamondImage image="/hero/img1.jpeg" />
+                    </div>
+            </div>
+        </div>
+    )
 }
 
 export default StudentsLife
---
+++ src/utils/diamond.js
@@ -0,0 +1,13 @@
+const DiamondImage = ({ image }) => {
+  return (
+    <div className="w-40 h-40 overflow-hidden border-4 border-sky-950/60 shadow-lg rounded-[100%]
+    hover:scale-120 transition-transform duration-300 cursor-pointer">
+      <img
+        src={image}
+        alt=""
+        className="w-full h-full object-cover scale-150"
+      />
+    </div>
+  );
+};
+export default DiamondImage;
