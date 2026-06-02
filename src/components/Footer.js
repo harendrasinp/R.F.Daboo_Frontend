ec14d7148501af4c80872c0680b6cc443bda54d0
harendrsainh <haren111990@gmail.com>
2026-06-02 20:59:09 +0530

    update mobile footer trial

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,33 +7,37 @@ import { FaXTwitter } from "react-icons/fa6";
 const Footer = () => {
   return (
     <div className="flex justify-center bg-slate-800 text-white text-sm p-5">
-      <div className="flex justify-center gap-1 md:w-1/2 md:justify-evenly">
-        <div className="flex flex-col items-start justify-start border-r-2 border-gray-600 md:pr-8">
-          <h1 className="text-lg font-bold mb-2">Quick Links</h1>
-          {navItems.map((item, index) => (
-            <div key={index} className="flex items-center gap-1">
-              <Square className="w-2 h-2 shrink-0 text-amber-300 bg-amber-300 " />
-              <a href={item.path} className="hover:underline cursor-pointer flex flex-col items-start justify-start text-sm p-1">
-                {item.name}
-              </a>
-            </div>
-          ))}
-        </div>
-        <div className="w-45 border-r-2 border-gray-600">
-          <h1 className="text-lg font-bold mb-2 text-left">Contact Us</h1>
-          {contactData.map((item, index) => (
-            <div key={index} className="flex flex-col items-start justify-start text-sm p-1">
-              <div className="flex items-center gap-1">
+      <div className="flex flex-col md:flex-row justify-center md:w-1/2 md:justify-evenly">
+        <div className="w-88 flex items-start justify-between md:w-90">
+          <div className="w-35 flex flex-col items-start justify-start border-r-2 border-gray-600">
+            <h1 className="text-lg font-bold mb-2">Quick Links</h1>
+            {navItems.map((item, index) => (
+              <div key={index} className="flex items-center gap-1">
                 <Square className="w-2 h-2 shrink-0 text-amber-300 bg-amber-300 " />
-                <p className="font-semibold">{item.title}</p>
+                <a href={item.path} className="hover:underline cursor-pointer flex flex-col items-start justify-start text-sm p-1">
+                  {item.name}
+                </a>
               </div>
-              <p className="text-left">{item.data}</p>
-            </div>
-          ))}
+            ))}
+          </div>
+          <div className="w-45 md:border-r-2 border-gray-600 ">
+            <h1 className="text-lg font-bold mb-2 text-left">Contact Us</h1>
+            {contactData.map((item, index) => (
+              <div key={index} className="flex flex-col items-start justify-start text-sm p-1">
+                <div className="flex items-center gap-1">
+                  <Square className="w-2 h-2 shrink-0 text-amber-300 bg-amber-300 " />
+                  <p className="font-semibold">{item.title}</p>
+                </div>
+                <p className="text-left">{item.data}</p>
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="flex flex-col items-start justify-start">
-          <h1 className="text-lg font-bold mb-2 flex text-left">Follow Us</h1>
-          <div className="flex flex-col items-start justify-center gap-4">
+        <div className="flex flex-col justify-center items-center border-t-2 border-gray-600 md:border-t-0 md:items-start md:justify-start">
+          <div className="mb-2">
+            <h1 className="text-lg font-bold">Follow Us</h1>
+          </div>
+          <div className="flex md:flex-col items-start justify-center gap-4">
             <a href="https://www.facebook.com/yourpage" target="_blank" rel="noopener noreferrer" className="text-white hover:text-blue-500 flex gap-2">
               <FaFacebook size={24} />
               Facebook
