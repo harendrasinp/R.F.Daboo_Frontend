f4900433740e278dac02e945a9f163685c9d3f00
harendrsainh <haren111990@gmail.com>
2026-06-10 20:07:48 +0530

    add redux toolkit

--- src/data/PrincipalData.js
+++ src/data/PrincipalData.js
@@ -1,36 +1,36 @@
 export const principalData = [
     {
         id: 1,
-        name: "Harendrasinh Parmar",
+        name: "Avni rajput",
         school: "School Name 1",
         image: "/Principls/p1.jpg"
     },
     {     id: 2,
-        name: "Kuldeepsinh Parmar",
+        name: "Kalpesh Rajput",
         school: "School Name 2",
         image: "/Principls/p2.jpg" 
     },
     {
         id: 3,  
-        name: "Kalpesh Rajput",
+        name: "Divyanshu Rajput",
         school: "School Name 3",
         image: "/Principls/p3.jpg"
     },
     {
         id: 4,  
-        name: "Baldevsinh Rajput",
+        name: "Rajendrasinh Parmar",
         school: "School Name 4",
         image: "/Principls/p4.jpg"
     },
     {
         id: 5,  
-        name: "Dharmendrasinh Parmar",
+        name: "Sneha Parmar",
         school: "School Name 5",
         image: "/Principls/p5.jpg"
     },
     {
         id: 6,
-        name: "Rajendrasinh Parmar",
+        name: "Harendrasinh Parmar",
         school: "School Name 6",
         image: "/Principls/p6.jpg"
     }
