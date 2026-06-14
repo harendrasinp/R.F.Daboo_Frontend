120db32f8fe595df48b3f0aa9afe991908b62633
harendrsainh <haren111990@gmail.com>
2026-06-14 11:33:25 +0530

    page protect

--- src/proxy.js
+++ src/proxy.js
@@ -1,6 +1,7 @@
 import {NextResponse } from "next/server";
 
 export function proxy(request) {
+  console.log("Running Application proxy")
   const token = request.cookies.get("token")?.value;
   if (!token) {
     return NextResponse.redirect(
