37b091ca85ec6c955a83dc139b1b9a87991ca300
harendrasinh <haren111990@gmail.com>
2026-08-02 11:35:15 +0530

    update gallery bg image for mobiel view

--- src/app/(website)/gallery/page.js
+++ src/app/(website)/gallery/page.js
@@ -10,15 +10,15 @@ const Gallery = () => {
     dispatch(GetImageDataThunk())
   }, [])
   return (
-    <div className="relative bg-[url('/gallery/gallerybg1.png')] bg-cover bg-center w-full min-h-screen md:bg-white flex felx-col justify-center flex-wrap">
-      <div className='text-2xl font-bold text-gray-800 mt-5 '>Events Gallery</div>
-      <div className='w-full grid grid-cols-1 md:grid-cols-4 gap-2 px-2 justify-items-center mb-5'>
-        {EventName?.EventData?.map((Event, index) => (
-          <div key={index} className='w-full h-45 md:w-55 md:h-55 bg-black/80 flex flex-col justify-center items-center text-center rounded'>
-            <h1 className='text-white text-lg font-semibold'>{Event}</h1>
-          </div>
-        ))}
-      </div>
+    <div className="relative bg-[url('/gallery/gallerybg2.png')] md:bg-[url('/gallery/gallerybg1.png')] bg-cover bg-center w-full min-h-screen md:bg-white flex felx-col justify-center flex-wrap">
+        <div className='text-2xl font-bold text-gray-800 mt-5 '>Events Gallery</div>
+        <div className='w-full grid grid-cols-1 md:grid-cols-4 gap-2 px-2 justify-items-center mb-5'>
+          {EventName?.EventData?.map((Event, index) => (
+            <div key={index} className='w-full h-45 md:w-55 md:h-55 bg-black/80 flex flex-col justify-center items-center text-center rounded'>
+              <h1 className='text-white text-lg font-semibold'>{Event}</h1>
+            </div>
+          ))}
+        </div>
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,30 +2,30 @@
 import { navItems } from "../data/navItems"
 import Link from "next/link"
 import Image from "next/image"
-import { openLoginModal} from "@/redux/slices/Loginslice"
+import { openLoginModal } from "@/redux/slices/Loginslice"
 import { useDispatch } from "react-redux"
-import {TextAlignJustify} from "lucide-react"
+import { TextAlignJustify } from "lucide-react"
 import Marque from "./Marque"
 import MobileSlideMenu from "./MobileSlideMenu"
 import { useState } from "react"
 
 const Header = () => {
   // -----------------------------States-------------------------------
-  const [isOpenMobileMenu,setIsOpenMobileMenu]=useState(false)
-  const [isOpenLogin,setIsOpenLogin]=useState(false)
-// ------------------------Dispatch---------------------------------
-  const dispatch=useDispatch()
-// ---------------------------------Mobile Menu Open Close function--------------------------
-  const mobileMenuhandler=()=>{
+  const [isOpenMobileMenu, setIsOpenMobileMenu] = useState(false)
+  const [isOpenLogin, setIsOpenLogin] = useState(false)
+  // ------------------------Dispatch---------------------------------
+  const dispatch = useDispatch()
+  // ---------------------------------Mobile Menu Open Close function--------------------------
+  const mobileMenuhandler = () => {
     setIsOpenMobileMenu(!isOpenMobileMenu)
   }
-// -------------------------------Login Modal Open Close Function---------------------------
+  // -------------------------------Login Modal Open Close Function---------------------------
   // const LoginModalFunction=()=>{
   //   setIsOpenLogin(!isOpenLogin)
   // }
-// ----------------------------------------Return-----------------------------------------
+  // ----------------------------------------Return-----------------------------------------
   return (
-    <div className="relative bg-[url('/header/headerbg1.png')] bg-cover bg-no-repeat bg-center w-full md:bg-white">
+    <div className="relative bg-[url('/header/headerbg2.png')] md:bg-[url('/header/headerbg1.png')] bg-cover bg-no-repeat bg-center w-full md:bg-white">
       <div className="flex items-end px-2 py-1">
         <div className="flex items-end px-4 md:py-2 w-25 h-25 md:w-35 md:h-35">
           <Image
@@ -36,34 +36,34 @@ const Header = () => {
             className="object-cover md:w-full md:h-full"
           />
         </div>
-          <div className="absolute top-15 right-1 md:block w-8 h-8 md:w-18 md:h-18 md:top-4 md:right-0">
-             <Image src="/header/rfimage2.png" alt="header image" width={100} height={100} className="w-full h-auto object-cover" />
-          </div>
+        <div className="absolute top-15 right-1 md:block w-8 h-8 md:w-18 md:h-18 md:top-4 md:right-0">
+          <Image src="/header/rfimage2.png" alt="header image" width={100} height={100} className="w-full h-auto object-cover" />
+        </div>
         <div className="flex-col items-center text-BlueNavyColor text-sm font-bold md:text-2xl">
           <h1 >Shree R. F. Daboo </h1>
           <h1>Education Society (VYARA)</h1>
         </div>
-{/* --------------------------Hamburger icon--------------------------------------------------------- */}
+        {/* --------------------------Hamburger icon--------------------------------------------------------- */}
         <div>
-            <TextAlignJustify onClick={()=>mobileMenuhandler()}className="md:hidden absolute top-4 right-4 text-blue-950 font-bold w-10 h-10"/>
+          <TextAlignJustify onClick={() => mobileMenuhandler()} className="md:hidden absolute top-4 right-4 text-blue-950 font-bold w-10 h-10" />
         </div>
-      </div> 
+      </div>
       {<MobileSlideMenu openStatus={isOpenMobileMenu} openmenufunction={mobileMenuhandler} />}
-{/* -------------------------------Marque-------------------------------------------------------------- */}
+      {/* -------------------------------Marque-------------------------------------------------------------- */}
       <Marque />
-{/* ------------------------------Menu Bar-------------------------------------------------------------- */}
+      {/* ------------------------------Menu Bar-------------------------------------------------------------- */}
       <div className="hidden w-full bg-gray-800 md:flex justify-center items-center space-x-6 py-1">
         {navItems.map((item, index) => (
           <Link key={index} href={item.path} className="text-white hover:text-orange-300">
             {item.name}
           </Link>
         ))}
-{/* -------------------------------Login Button---------------------------------------------------------- */}
-        <button onClick={()=>dispatch(openLoginModal())} className="text-white px-3 py-1 rounded hover:text-orange-300 cursor-pointer">
+        {/* -------------------------------Login Button---------------------------------------------------------- */}
+        <button onClick={() => dispatch(openLoginModal())} className="text-white px-3 py-1 rounded hover:text-orange-300 cursor-pointer">
           Login
         </button>
       </div>
-{/* --------------------------------Login Modal-------------------------------------- */}
+      {/* --------------------------------Login Modal-------------------------------------- */}
     </div>
   )
 }
