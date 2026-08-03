7a25075287e36261e3b2d40f9fb8c47acf7de4bb
harendrsainh <haren111990@gmail.com>
2026-08-03 23:16:25 +0530

    gallery/year get on page by slug

--- src/app/(website)/gallery/[EventName]/page.js
+++ src/app/(website)/gallery/[EventName]/page.js
@@ -7,7 +7,7 @@ const GalleryEvent = async ({ params }) => {
     console.log("data", data)
     return (
         <div className="relative bg-[url('/gallery/gallerybg2.png')] md:bg-[url('/gallery/gallerybg1.png')] bg-cover bg-center w-full min-h-screen md:bg-white flex felx-col justify-center flex-wrap">
-            <div className="mt-5 flex justify-center items-center gap-5">
+            <div className="mt-5 flex flex-col justify-center items-center gap-5 md:flex-row">
                  {
                     data?.YearData?.map((item, index) => (
                         <div key={index} className='w-full h-45 md:w-55 md:h-55 bg-black/80 flex flex-col justify-center items-center text-center rounded cursor-pointer'>
